624aac26bd9df2aea9f692cd66f14f1fccb084c5
M-Pragati06 <147226045+M-Pragati06@users.noreply.github.com>
2025-04-18 16:10:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import MultiStepForm from './pages/MultiStepForm';
 
 function App() {
   return (
-    <div className="min-h-screen py-12 px-4 sm:px-6 lg:px-8" style={{backgroundImage: "url('src/assets/bg.avif')", backgroundSize: 'cover'}}>
+    <div className="min-h-screen py-12 px-4 sm:px-6 lg:px-8" style={{backgroundImage: "url('src/assets/bg.jpg')", backgroundSize: 'cover'}}>
       <MultiStepForm />
     </div>
   );
