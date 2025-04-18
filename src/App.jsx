a43b88826d55c65b8b73d7aa272951128b4963d3
M-Pragati06 <147226045+M-Pragati06@users.noreply.github.com>
2025-04-18 16:22:15 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import MultiStepForm from './pages/MultiStepForm';
+import bgImage from './assets/bg.avif';
+import React from 'react';
 
 function App() {
   return (
-    <div className="min-h-screen py-12 px-4 sm:px-6 lg:px-8" style={{backgroundImage: "url('src/assets/bg.jpg')", backgroundSize: 'cover'}}>
+    <div 
+      className="min-h-screen py-12 px-4 sm:px-6 lg:px-8" 
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+      }}
+    >
       <MultiStepForm />
     </div>
   );
